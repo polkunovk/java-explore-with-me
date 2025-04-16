19c7ad218cf61f7a7d0e087b16dfe5f4b72f795b
Konstantin <polkunovk@yandex.ru>
2025-04-16 13:01:42 +0300

    checkstyle

--- main-service/src/main/java/ru/practicum/error/exception/AccessDeniedException.java
+++ main-service/src/main/java/ru/practicum/error/exception/AccessDeniedException.java
@@ -1,6 +1,6 @@
 package ru.practicum.error.exception;
 
-public class AccessDeniedException extends RuntimeException{
+public class AccessDeniedException extends RuntimeException{ 
     public AccessDeniedException(String message) {
         super(message);
     }
